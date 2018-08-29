c109d1f9337c0df7dbdf51be4385e79daf31bb85
= <=>
2018-08-30 07:39:40 +0800

    修改organization app里面课程机构，主要是分页功能

--- muxue/organization/urls.py
+++ muxue/organization/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from organization import views
 
 urlpatterns = [
-	path("org_list/",views.Org_list.as_view())
+	re_path("org_list/(?P<nid>\d*)",views.Org_list.as_view())
 	
 ]
 # Create your tests here.
--- muxue/organization/views.py
+++ muxue/organization/views.py
@@ -4,16 +4,24 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from .models import CourseOrg,CityDict
+from django.core.paginator import Paginator
+
 
 class Org_list(View):
 	"""
 	课程机构列表功能
 	"""
-	def get(self,request):
+	def get(self,request,nid):
 		#取出课程机构
-		all_orgs = CourseOrg.objects.all()
+		all_orgs = CourseOrg.objects.all().order_by("id")  #对数据进行排序，否则分页会报错，
 		#取出城市地址
+		orgs_total = len(all_orgs)
 		all_citys = CityDict.objects.all()
-		return render(request,"organization/org-list.html",{"all_orgs":all_orgs,"all_citys":all_citys})
+		paginator = Paginator(all_orgs,5)  #将取出数据每5条分为一页
+		yeshu = paginator.page_range    #得到页码的一个range列表
+		if nid == "":
+			nid = 1
+		page = paginator.page(nid)  #展示每页数据
+		return render(request,"organization/org-list.html",locals())
 
 # Create your views here.
